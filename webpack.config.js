c5cc20be2b72c739af51eafefe8096ef239e93eb
Scott de Jonge <dejonge.scott@gmail.com>
2016-02-10 16:24:32 +1000

    Init webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var postcssImport = require('postcss-import');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+      }
+    ]
+  },
+  postcss: function(webpack) {
+    return [
+      autoprefixer({
+        browsers: ['last 2 versions']
+      }),
+      postcssImport({
+        path: './src/css/*.css',
+        addDependencyTo: webpack
+      }),
+    ];
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', {
+      allChunks: true
+    })
+  ]
+};
